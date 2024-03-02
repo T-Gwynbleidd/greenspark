62f9f4d276320141891fd3ec7f48dbbccc7f53af
Tomas <tomas@wildwebsite.design>
2024-03-02 13:11:57 +0000

    Added svg assets, installed package to process svg

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
+import VueSvgInlinePlugin from 'vue-svg-inline-plugin';
+import App from './App.vue';
+import store from './store';
 
-createApp(App).use(store).mount('#app')
+createApp(App).use(store).use(VueSvgInlinePlugin).mount('#app');
