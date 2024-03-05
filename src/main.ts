0ead33e2b182489c25f66113e642bf11a57864d2
Tomas <tomas@wildwebsite.design>
2024-03-05 15:44:49 +0000

    Added cabin font

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { createApp } from 'vue';
+import VueSvgInlinePlugin from 'vue-svg-inline-plugin';
 import App from './App.vue';
 import { store, key } from './store';
+import './styles/fonts.scss';
 
 // Note: Passed loadDuration to allow for spinner to appear during browser access for a nicer visual, but not during testing (would require awaiting each test for the spinner to end)
-createApp(App, { loadDuration: 2000 }).use(store, key).mount('#app');
+createApp(App, { loadDuration: 2000 })
+  .use(store, key)
+  .use(VueSvgInlinePlugin)
+  .mount('#app');
