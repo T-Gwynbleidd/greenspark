cfe90fe0f02ccb880cba87b488521521487cc1de
Tomas <tomas@wildwebsite.design>
2024-03-06 13:28:08 +0000

    Tidied up unused assets

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  transformIgnorePatterns: ['/node_modules/(?!vue-svg-inline-plugin)'],
   transform: {
     '^.+\\.vue$': './vue3JestHack.js',
   },
